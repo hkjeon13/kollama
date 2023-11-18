3f4e99f16a5bb5ad39abd9b49c923a31ffab6740
psyche <hkjeon13@gmail.com>
2023-11-18 14:57:57 +0900

    add test case for the custom.py

--- get_initial_llama.py
+++ get_initial_llama.py
@@ -15,7 +15,7 @@ def main():
     model_args, build_args = parser.parse_args_into_dataclasses()
     target_params = set(signature(LlamaConfig.__init__).parameters.keys() - {"self"})
     _target_params = {key: value for key, value in model_args.__dict__.items() if key in target_params}
-    config = LlamaParams(**_target_params)
+    config = LlamaConfig(**_target_params)
     model = LlamaModel(config)
     model.save_pretrained(build_args.output_dir)
     print("Done!")
