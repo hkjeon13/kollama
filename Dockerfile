9ede0d73b3de20b817483935d2b047ad151c4869
psyche <psyche@lotte.net>
2023-11-22 10:31:38 +0900

    fix errors on Params

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,15 @@
 FROM huggingface/transformers-pytorch-cpu
-RUN apt-get install git git-lfs curl wget -y 
+RUN apt update -y 
+RUN apt-get update -y  
+RUN apt-get install git git-lfs curl wget software-properties-common vim -y  
+RUN apt-get install build-essential -y  
+RUN add-apt-repository ppa:deadsnakes/ppa -y  
+RUN apt install python3.10 -y  
+RUN apt install python3.10-distutils -y  
+RUN apt install python3.10-dev -y  
+RUN update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1  
+RUN update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1  
+RUN curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10  -y  
+RUN pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 -y  
+RUN pip3 install transformers datasets accelerate evaluate deepspeed slacker seaborn scikit-learn -y  
+RUN pip3 install fsspec==2023.6.0 datasets==2.12.0 huggingface-hub==0.17.0 -y
---
+++ download_datasets.py
@@ -0,0 +1,53 @@
+import json
+import os
+from dataclasses import dataclass, field
+from transformers import HfArgumentParser
+from datasets import load_dataset
+from tqdm import tqdm
+
+
+def load_json(path:str) -> dict:
+    with open(path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+
+@dataclass
+class DownloadParams:
+    data_info_path: str = field(
+        default="data_info.json",
+        metadata={"help": "The data info path to use"}
+    )
+
+    output_dir: str = field(
+        default="data",
+        metadata={"help": "The output dir to use"}
+    )
+
+
+def trim_name(name: str) -> str:
+    name = name.replace("/", "_")
+    name = name.replace(":", "_")
+    name = name.replace(",", "_")
+    name = name.replace(".", "_")
+    return name
+
+
+def main():
+    parser = HfArgumentParser((DownloadParams,))
+    download_params = parser.parse_args_into_dataclasses()[0]
+
+    data_info = load_json(download_params.data_info_path)
+    for info in tqdm(data_info):
+        dataset = load_dataset(**info["data_name_or_path"].split(","), use_auth_token=info["data_auth_token"])
+        target_dir = os.path.join(download_params.output_dir, trim_name(info["data_name_or_path"]))
+        for key, value in dataset.items():
+            num_shard = (value.size_in_bytes // 1e+8) + 1
+            for i in range(num_shard):
+                path = os.path.join(target_dir, f"{key}-{i:05d}-of-{num_shard:05d}.parquet")
+                value.shard(num_shard, i, contiguous=True).to_parquet(path)
+
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
